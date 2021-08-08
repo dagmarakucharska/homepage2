e60e060415f24e7b446b50b2bd0c8a51f725685c
dagmarakucharska <carribean09@gmail.com>
2021-08-08 16:00:43 +0200

    format document

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 /*alert ('witam na mojej stronie!');*/
 
-const name = 'Dagmara';
+const name = "Dagmara";
 const age = 29;
 
 console.log(name);
 console.log(age);
 
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const heading = document.querySelector('.second-header');
+console.log(heading);
+
+heading.innerHTML = `Nazywam się ${name} i mam ${age} lat`;
